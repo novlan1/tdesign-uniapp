ab565b222e7cce294945a67e74b9b3b13b45e067
novlan1 <1576271227@qq.com>
2025-10-04 09:55:05 +0800

    chore: update badge

--- packages/site/src/pages-more/badge/size/index.vue
+++ packages/site/src/pages-more/badge/size/index.vue
@@ -1,32 +1,43 @@
 <template>
-    <view>
-        <view class="demo-desc">Large</view>
-
-        <view class="block">
-            <t-avatar icon="user" size="large" badge-props="" />
-        </view>
-
-        <view class="demo-desc">Middle</view>
-
-        <view class="block">
-            <t-avatar icon="user" badge-props="" />
-        </view>
+  <view>
+    <view class="demo-desc">
+      Large
     </view>
+
+    <view class="block">
+      <t-avatar
+        icon="user"
+        size="large"
+        badge-props=""
+      />
+    </view>
+
+    <view class="demo-desc">
+      Middle
+    </view>
+
+    <view class="block">
+      <t-avatar
+        icon="user"
+        badge-props=""
+      />
+    </view>
+  </view>
 </template>
 
 <script>
 import tBadge from 'tdesign-uniapp/badge/badge';
 import tAvatar from 'tdesign-uniapp/avatar/avatar';
 export default {
-    components: {
-        tBadge,
-        tAvatar
-    },
-    data() {
-        return {};
-    },
-    methods: {},
-    created: function () {}
+  components: {
+    tBadge,
+    tAvatar,
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
 };
 </script>
 <style>
