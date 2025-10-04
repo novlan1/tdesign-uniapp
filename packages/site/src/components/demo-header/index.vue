5889b3cb84d059d9362c62ab49cc0b7566f0a8a8
novlan1 <1576271227@qq.com>
2025-10-04 16:04:40 +0800

    feat: add demo-header

---
+++ packages/site/src/components/demo-header/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <TNoticeBar
+      visible
+      :content="notice"
+    />
+    <view class="demo-title">
+      {{ title }}
+    </view>
+    <view class="demo-desc">
+      {{ desc }}
+      <slot name="desc" />
+    </view>
+  </div>
+</template>
+<script>
+import TNoticeBar from 'tdesign-uniapp/notice-bar/notice-bar.vue';
+
+export default {
+  name: 'DemoHeader',
+  components: {
+    TNoticeBar,
+  },
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    desc: {
+      type: String,
+      default: '',
+    },
+    notice: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+<style lang="scss" scoped src="./index.less">
+</style>
--- packages/site/src/main.ts
+++ packages/site/src/main.ts
@@ -1,10 +1,12 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
 import DemoBlock from './components/demo-block/index.vue';
+import DemoHeader from './components/demo-header/index.vue';
 
 export function createApp() {
   const app = createSSRApp(App);
   app.component('t-demo', DemoBlock);
+  app.component('t-demo-header', DemoHeader);
   return {
     app,
   };
