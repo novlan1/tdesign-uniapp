77d3e01dec87e41dcf33702f1f2dfd01df5f7898
novlan1 <1576271227@qq.com>
2025-10-14 17:21:24 +0800

    feat: update content

--- packages/site/web/stackblitz/index.vue
+++ packages/site/web/stackblitz/index.vue
@@ -1,21 +1,67 @@
 <template>
   <td-tooltip trigger-type="hover">
-    <form ref="formRef" method="post" action="https://stackblitz.com/run" target="_blank" @click="submit">
-      <input type="hidden" name="project[files][src/demo.vue]" :value="code" />
-      <input type="hidden" name="project[files][src/index.css]" :value="styleContent" />
-      <input type="hidden" name="project[files][src/main.js]" :value="mainJsContent" />
-      <input type="hidden" name="project[files][index.html]" :value="htmlContent" />
-      <input type="hidden" name="project[files][package.json]" :value="packageJSONContent" />
-      <input type="hidden" name="project[files][vite.config.js]" :value="viteConfigContent" />
-      <input type="hidden" name="project[files][.stackblitzrc]" :value="stackblitzRc" />
-      <input type="hidden" name="project[template]" value="node" />
+    <form
+      ref="formRef"
+      method="post"
+      action="https://stackblitz.com/run"
+      target="_blank"
+      @click="submit"
+    >
+      <input
+        type="hidden"
+        name="project[files][src/pages/index/index.vue]"
+        :value="code"
+      >
+      <input
+        type="hidden"
+        name="project[files][index.html]"
+        :value="HTML_CONTENT"
+      >
+      <input
+        type="hidden"
+        name="project[files][src/main.js]"
+        :value="MAN_JS_CONTENT"
+      >
+      <input
+        type="hidden"
+        name="project[files][src/pages.json]"
+        :value="PAGES_JSON_CONTENT"
+      >
+      <input
+        type="hidden"
+        name="project[files][src/manifest.json]"
+        :value="MANIFEST_JSON_CONTENT"
+      >
+      <input
+        type="hidden"
+        name="project[files][package.json]"
+        :value="PACKAGE_JSON_CONTENT"
+      >
+      <input
+        type="hidden"
+        name="project[files][vite.config.js]"
+        :value="VITE_CONFIG_CONTENT"
+      >
+      <input
+        type="hidden"
+        name="project[files][.stackblitzrc]"
+        :value="STACKBLITZ_RZ"
+      >
+      <input
+        type="hidden"
+        name="project[template]"
+        value="node"
+      >
 
       <div class="action-online">
-        <svg viewBox="0 0 28 28" height="20">
+        <svg
+          viewBox="0 0 28 28"
+          height="20"
+        >
           <path
             fill="currentColor"
             d="M12.747 16.273h-7.46L18.925 1.5l-3.671 10.227h7.46L9.075 26.5l3.671-10.227z"
-          ></path>
+          />
         </svg>
       </div>
     </form>
@@ -26,12 +72,14 @@
 <script>
 import { defineComponent, ref } from 'vue';
 import {
-  htmlContent,
-  mainJsContent,
-  styleContent,
-  packageJSONContent,
-  stackblitzRc,
-  viteConfigContent,
+  HTML_CONTENT,
+  MAN_JS_CONTENT,
+  MANIFEST_JSON_CONTENT,
+  PAGES_JSON_CONTENT,
+  APP_VUE_CONTENT,
+  STACKBLITZ_RZ,
+  VITE_CONFIG_CONTENT,
+  PACKAGE_JSON_CONTENT,
 } from './content';
 
 export default defineComponent({
@@ -44,19 +92,25 @@ export default defineComponent({
 
   setup() {
     const data = {
-      htmlContent,
-      mainJsContent,
-      styleContent,
-      stackblitzRc,
-      viteConfigContent,
-      packageJSONContent,
+      HTML_CONTENT,
+      MAN_JS_CONTENT,
+      MANIFEST_JSON_CONTENT,
+      PAGES_JSON_CONTENT,
+      APP_VUE_CONTENT,
+      STACKBLITZ_RZ,
+      VITE_CONFIG_CONTENT,
+      PACKAGE_JSON_CONTENT,
     };
     const match = window.location.hostname.match(/preview-pr-(\d+)-tdesign-mobile-vue.surge.sh/);
     if (match) {
-      const packageJSON = JSON.parse(packageJSONContent);
-      packageJSON.dependencies['tdesign-mobile-vue'] =
-        `https://pkg.pr.new/Tencent/tdesign-mobile-vue/tdesign-mobile-vue@${match[1]}`;
-      data.packageJSONContent = JSON.stringify(packageJSON, null, 2);
+      let packageJSON = {};
+      try {
+        packageJSON = JSON.parse(PACKAGE_JSON_CONTENT);
+      } catch (e) {
+      }
+
+      packageJSON.dependencies['tdesign-mobile-vue'] = `https://pkg.pr.new/Tencent/tdesign-mobile-vue/tdesign-mobile-vue@${match[1]}`;
+      data.PACKAGE_JSON_CONTENT = JSON.stringify(packageJSON, null, 2);
     }
     const formRef = ref(null);
 
