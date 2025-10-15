31a2f2888f08c0d59975ab6eb0d4e54d6ed0d623
novlan1 <1576271227@qq.com>
2025-10-16 00:25:52 +0800

    chore: update deploy script

---
+++ packages/demo/script/deploy/mine-doc.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const { execCommand } = require('t-comm');
+
+require('./local.env')();
+const args = process.argv.slice(2);
+
+const DEPLOY_CONFIG = {
+  publishBash: './node_modules/t-comm/bin/publish.sh',
+  distDir: path.resolve(__dirname, '../../../site/dist'),
+  hostTargetDir: '/usr/share/nginx/html/tdesign-uniapp/',
+  hostName: args[0] || process.env.HOST_NAME,
+  hostPwd: args[0] || process.env.HOST_PWD,
+  HOST_PORT: 22,
+  root: process.cwd(),
+};
+
+async function main() {
+  console.log('[DEPLOY_CONFIG]', DEPLOY_CONFIG);
+
+  if (!DEPLOY_CONFIG.hostPwd || !DEPLOY_CONFIG.hostName) {
+    throw new Error('缺少环境变量');
+  }
+
+  execCommand(
+    `sh ${DEPLOY_CONFIG.publishBash} ${DEPLOY_CONFIG.distDir} ${DEPLOY_CONFIG.hostTargetDir} ${DEPLOY_CONFIG.hostName} ${DEPLOY_CONFIG.hostPwd} ${DEPLOY_CONFIG.HOST_PORT}`,
+    DEPLOY_CONFIG.root,
+    'inherit',
+  );
+}
+
+main();
