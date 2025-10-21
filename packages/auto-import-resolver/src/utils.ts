8dead1b2a66135fb8c97989a884c752cd04e6cad
novlan1 <1576271227@qq.com>
2025-10-21 17:54:59 +0800

    feat: add tdesign-uniapp-auto-import-resolver

---
+++ packages/auto-import-resolver/src/utils.ts
@@ -0,0 +1,21 @@
+import type { FilterPattern } from 'unplugin-utils';
+
+export function isExclude(name: string, exclude?: FilterPattern): boolean {
+  if (!exclude) return false;
+
+  if (typeof exclude === 'string') return name === exclude;
+
+  if (exclude instanceof RegExp) return !!name.match(exclude);
+
+  if (Array.isArray(exclude)) {
+    for (const item of exclude) {
+      if (name === item || name.match(item)) return true;
+    }
+  }
+  return false;
+}
+
+export function hyphenate(str: string) {
+  const hyphenateRE = /\B([A-Z])/g;
+  return str.replace(hyphenateRE, '-$1').toLowerCase();
+}
