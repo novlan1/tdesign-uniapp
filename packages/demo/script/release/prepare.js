34bc151db968632e4a6712b90b007eecac32abe7
novlan1 <1576271227@qq.com>
2025-11-16 16:15:33 +0800

    feat: add chat types

--- packages/demo/script/release/prepare.js
+++ packages/demo/script/release/prepare.js
@@ -5,29 +5,60 @@ const { deleteFolder } = require('t-comm');
 const { config } = require('./config');
 const { copy } = require('./core.js');
 
-
 async function main() {
-  deleteFolder(config.targetDir);
-  const list = glob.sync(config.sourceGlob, {
+  const {
+    targetDir,
+    sourceGlob,
+    sourceDir,
+
+    chatTargetDir,
+    chatSourceDir,
+    chatSourceGlob,
+  } = config;
+
+  await prepareOne({
+    targetDir,
+    sourceGlob,
+    sourceDir,
+  });
+  await prepareOne({
+    targetDir: chatTargetDir,
+    sourceGlob: chatSourceGlob,
+    sourceDir: chatSourceDir,
+  });
+}
+
+async function prepareOne({
+  targetDir,
+  sourceGlob,
+  sourceDir,
+}) {
+  deleteFolder(targetDir);
+  const list = glob.sync(sourceGlob, {
     ignore: '**/{node_modules,_example}/**/*',
     nodir: true,
     dot: true,
   });
 
   for (const item of list) {
-    const relativePath = path.relative(config.sourceDir, item);
+    const relativePath = path.relative(sourceDir, item);
     const {
       relativeTargetByCwd,
       relativeSourceByCwd,
     } = await copy({
       relativePath,
       filePath: item,
-      config,
+      config: {
+        targetDir,
+        sourceGlob,
+        sourceDir,
+      },
     });
     console.log(`[Wrote] done! \nFrom ${relativeSourceByCwd} to ${relativeTargetByCwd}`);
   }
 
   console.log(`[Wrote] done! Length is ${list.length}!`);
 }
+
 
 main();
