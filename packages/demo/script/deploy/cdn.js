aa8b67a3f601c21e7574490f8c5c969ff1d960b9
novlan1 <1576271227@qq.com>
2025-11-16 15:07:20 +0800

    feat: 优化chat-site (#67)

--- packages/demo/script/deploy/cdn.js
+++ packages/demo/script/deploy/cdn.js
@@ -12,18 +12,38 @@ const COS_CONFIG = {
   region: 'ap-guangzhou',
   secretId: args[0] || process.env.COS_SECRET_ID,
   secretKey: args[1] || process.env.COS_SECRET_KEY,
+  isChat: args[2] == 1,
+};
+
+const CONFIG = {
+  UNIAPP_SITE_GLOB: './packages/site/dist/**/*',
+  UNIAPP_CHAT_SITE_GLOB: './packages/site-chat/dist/**/*',
+
+  UNIAPP_PREFIX: 'tdesign-uniapp',
+  UNIAPP_CHAT_PREFIX: 'tdesign-uniapp-chat',
+
+  UNIAPP_SITE_PACKAGE_NAME: 'site',
+  UNIAPP_CHAT_SITE_PACKAGE_NAME: 'site-chat',
 };
 
 
 async function uploadCdn() {
-  const list = glob.sync('./packages/site/dist/**/*', {
+  const list = glob.sync(COS_CONFIG.isChat ? CONFIG.UNIAPP_SITE_GLOB
+    : CONFIG.UNIAPP_CHAT_SITE_GLOB, {
     ignore: '**/*.html',
     nodir: true,
   });
 
   const files = list.map((item) => {
     const filePath = path.resolve(process.cwd(), item);
-    const key =  `tdesign-uniapp/${path.relative(path.resolve(process.cwd(), './packages/site/dist'), item)}`;
+    const key =  `${COS_CONFIG.isChat
+      ? CONFIG.UNIAPP_PREFIX
+      : CONFIG.UNIAPP_CHAT_PREFIX}/${path.relative(path.resolve(
+      process.cwd(),
+      `./packages/${COS_CONFIG.isChat
+        ? CONFIG.UNIAPP_SITE_PACKAGE_NAME
+        : CONFIG.UNIAPP_CHAT_SITE_PACKAGE_NAME}/dist`,
+    ), item)}`;
 
     return {
       key,
