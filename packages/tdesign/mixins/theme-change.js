c83ad61ad34f418d5114bb39b38dac4d131988f8
novlan1 <1576271227@qq.com>
2025-11-25 11:11:33 +0800

    feat(theme): 移除vue3语法，兼容vue2

--- packages/tdesign/mixins/theme-change.js
+++ packages/tdesign/mixins/theme-change.js
@@ -1,27 +1,4 @@
 import { appBaseInfo } from '../common/utils';
-// #ifdef VUE3
-import { ref, onBeforeMount } from 'vue';
-// #endif
-
-export const useTheme = () => {
-  const theme = ref('light');
-  const initTheme = () => {
-    theme.value = appBaseInfo.theme;
-    if (typeof uni.onThemeChange !== 'function') return;
-    uni.onThemeChange((t) => {
-      theme.value = t.theme;
-    });
-  };
-
-  onBeforeMount(() => {
-    initTheme();
-  });
-
-  return {
-    theme,
-  };
-};
-
 
 export const themeMixin = {
   data() {
